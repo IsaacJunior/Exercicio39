ebce0cb807a17029016e8879a13484cde5af793f
Isaac Junior <maisaacjr@yahoo.com.br>
2019-09-25 09:03:38 -0300

    Atualizado o exercicio 39 do curso

---
+++ Exercicio39/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio39.Entities
+{
+    class Department
+    {
+
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
